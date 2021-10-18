49f09bdb592ffd2fb1931edf9e17a6cab6b7f392
Ayana Murakami <murakami.ayana3@gmail.com>
2021-10-19 05:27:13 +0900

    requirement

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import data as d
 import pandas as pd
 import plotly.express as px
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
 import numpy as np
 
